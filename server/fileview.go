359c3a152c8034144fd5ade8bfd4c61bd0f9e90a
Svyatoslav Ilinskiy <stas@dropbox.com>
2017-07-25 01:11:59 -0700

    Allow specifying language server in index.json and pass it to frontend
    List of ports with compatible language servers is stored in `window.languageServerPorts`
    
    Note that code in this commit does not check that the language server is actually running on the provided port.
    
    lang_server property in the index.json would look something like this:
    ```
    "lang_servers":
        [
            {
                "port": 3141,
                "extensions": [
                    ".py"
                ]
            }
        ]
     ```

--- server/fileview.go
+++ server/fileview.go
@@ -56,12 +56,13 @@ type directoryListEntry struct {
 }
 
 type fileViewerContext struct {
-	PathSegments   []breadCrumbEntry
-	Repo           config.RepoConfig
-	Commit         string
-	DirContent     *directoryContent
-	FileContent    *sourceFileContent
-	ExternalDomain string
+	PathSegments        []breadCrumbEntry
+	Repo                config.RepoConfig
+	Commit              string
+	LanguageServerPorts []int
+	DirContent          *directoryContent
+	FileContent         *sourceFileContent
+	ExternalDomain      string
 }
 
 type sourceFileContent struct {
@@ -171,7 +172,7 @@ func buildDirectoryListEntry(treeEntry gitTreeEntry, pathFromRoot string, repo c
 	}
 }
 
-func buildFileData(relativePath string, repo config.RepoConfig, commit string) (*fileViewerContext, error) {
+func buildFileData(relativePath string, repo config.RepoConfig, commit string, langServersPorts []int) (*fileViewerContext, error) {
 	cleanPath := path.Clean(relativePath)
 	if cleanPath == "." {
 		cleanPath = ""
@@ -226,11 +227,12 @@ func buildFileData(relativePath string, repo config.RepoConfig, commit string) (
 	}
 
 	return &fileViewerContext{
-		PathSegments:   segments,
-		Repo:           repo,
-		Commit:         commit,
-		DirContent:     dirContent,
-		FileContent:    fileContent,
-		ExternalDomain: externalDomain,
+		PathSegments:        segments,
+		Repo:                repo,
+		Commit:              commit,
+		LanguageServerPorts: langServersPorts,
+		DirContent:          dirContent,
+		FileContent:         fileContent,
+		ExternalDomain:      externalDomain,
 	}, nil
 }
